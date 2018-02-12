060d72a7c7f3381510e8634b19fb1e04014830b1
Andrew L. Moore <slewsys@gmail.com>
2018-02-12 01:25:39 -0500

    Initial version of React.js demo.

---
+++ README.md
@@ -0,0 +1,23 @@
+## React.js demo
+- Lesson 1 implements a simple web app using HTML markup and JavaScript.
+- Lesson 2 converts the HTML markup in Lesson 1 using JavaScript's DOM API.
+- Lesson 3 is a React app that parallels the JavaScript from Lesson 2.
+- Lesson 4 converts the React DOM API of Lesson 3 to JSX, paralleling the HTML
+  markup of Lesson 1.
+
+Lessons 3 and 4 were created with [Facebook's Create React App](https://github.com/facebook/create-react-app).
+The following commands must be installed to run them:
+- [nodejs](https://nodejs.org/en/)  (v8.9.4)
+- [yarn](https://github.com/yarnpkg/yarn) (v1.3.2)
+- [npm](https://github.com/npm/npm) (5.6.0)
+
+To install dependencies for, e.g., Lesson 3, run:
+```
+cd lesson3
+yarn install
+```
+
+After its dependencies are installed, run Lesson 3 using:
+```
+yarn start
+```
---
+++ install-and-initialize-react-app.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+#
+# @(#) install-and-initialize-react-app.sh
+#
+declare prefix='/usr/local'
+
+: ${CREATE_REACT_APP:="${prefix}/bin/create-react-app"}
+: ${GIT:="${prefix}/bin/git"}
+: ${NODE:="${prefix}/bin/node"}
+: ${NPX:="${prefix}/bin/npx"}
+: ${YARN:="${prefix}/bin/yarn"}
+
+declare script_name=${0##*/}
+
+declare app_name=${1:-'react-app'}
+
+have_required_utilities ()
+{
+    for app in "$NODE" "$YARN" "$NPX" "$GIT"; do
+        if test ! -x "$app"; then
+            cat <<EOF
+$script_name: $app: No such file or directory
+Please install ${app##*/}, then try again.
+EOF
+            return 1
+        fi
+    done
+    return 0
+}
+
+install_create_react_app ()
+{
+    if test -x "$CREATE_REACT_APP"; then
+        return 0
+    fi
+    sudo $YARN global add create-react-app --prefix="$prefix"
+}
+
+initialize_react_app ()
+{
+    if ! $NPX create-react-app "$app_name" || ! cd "$app_name"; then
+        echo "$script_name: $app_name: No such file or directory."
+        return 1
+    fi
+    $GIT init .
+    $GIT add .
+    $YARN start
+}
+
+have_required_utilities || exit 1
+install_create_react_app || exit 1
+initialize_react_app || exit 1
