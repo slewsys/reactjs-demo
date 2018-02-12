3d64210cde6bbf634b3111637265e0074a154350
Andrew L. Moore <slewsys@gmail.com>
2018-02-12 14:36:06 -0500

    Code cleanup.

--- lesson1/lesson1.js
+++ lesson1/lesson1.js
@@ -1,30 +1,33 @@
+const returnKey = 13;
+
 let nameNode = document.getElementById("name");
 
 nameNode.addEventListener("keyup",
   e => {
-    const returnKey = 13;
-    e.preventDefault();
-    if (e.keyCode === returnKey) {
-      if (nameNode.value.length > 0) {
-        let sayHelloNode  = document.createElement('div');
-        let highlightNode = document.createElement('h2');
-        let greetingNode  = document.createTextNode(`Hello ${nameNode.value}!`);
-
-        highlightNode.appendChild(greetingNode);
-        sayHelloNode.appendChild(highlightNode);
-        sayHelloNode.className = "SayHello";
-
-        let spanNode  = document.getElementById("input");
-        let rootNode  = document.getElementById("root");
-        let rootNodes = rootNode.childNodes;
-
-        rootNodes.forEach(node => {
-          if (node.id !== "input") {
-            rootNode.removeChild(node);
-          }
-        });
-        rootNode.insertBefore(sayHelloNode, spanNode);
-        nameNode.value = "";
-      }
+    if (e.keyCode !== returnKey || nameNode.value.length <= 0) {
+      return;
     }
-  });
+
+    let sayHelloNode  = document.createElement('div');
+    let highlightNode = document.createElement('h2');
+    let greetingNode  = document.createTextNode(`Hello ${nameNode.value}!`);
+
+    highlightNode.appendChild(greetingNode);
+    sayHelloNode.appendChild(highlightNode);
+    sayHelloNode.className = "SayHello";
+
+    let rootNode  = document.getElementById("root");
+    let rootNodes = rootNode.childNodes;
+
+    rootNodes.forEach(node => {
+      if (node.id !== "input") {
+        rootNode.removeChild(node);
+      }
+    });
+
+    let spanNode  = document.getElementById("input");
+
+    rootNode.insertBefore(sayHelloNode, spanNode);
+    nameNode.value = "";
+  }
+);
