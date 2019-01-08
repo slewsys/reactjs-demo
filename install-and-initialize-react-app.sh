0beb1216966e9135d3ff4b9d91570c685812694c
Andrew L. Moore <slewsys@gmail.com>
2019-01-08 23:47:47 +0000

    install-and-initialize-react-app.sh: Use yarn.

--- install-and-initialize-react-app.sh
+++ install-and-initialize-react-app.sh
@@ -2,7 +2,13 @@
 #
 # @(#) install-and-initialize-react-app.sh
 #
-declare prefix='/usr/local'
+declare prefix='/usr'
+
+case "$(uname)" in
+    Darwin|*BSD)
+        prefix='/usr/local'
+        ;;
+esac
 
 : ${CREATE_REACT_APP:="${prefix}/bin/create-react-app"}
 : ${GIT:="${prefix}/bin/git"}
@@ -38,19 +44,13 @@ install_create_react_app ()
 
 initialize_react_app ()
 {
-    if test ! -x "$NPX"; then
-         sudo $YARN global add npx --prefix="$prefix" || exit 1
-    fi
-    if ! $NPX create-react-app "$app_name"; then
+    if ! $YARN create react-app "$app_name"; then
         echo "$script_name: $app_name: No such file or directory"
         return 1
     elif  ! cd "$app_name"; then
         echo "$script_name: $app_name: Permission denied"
         return 1
     fi
-    $GIT init .
-    $GIT add .
-    $YARN start
 }
 
 # Exec only if not sourced.
@@ -58,4 +58,5 @@ if test ."$0" = ."${BASH_SOURCE[0]}"; then
     have_required_utilities || exit 1
     install_create_react_app || exit 1
     initialize_react_app || exit 1
+    $YARN start
 fi
